f22081e8ebe2f807e1f5ef147902e7994b78f0ea
quote <quote@lv0.io>
2014-08-26 16:17:55 -0500

    added ground plane, and have cube sliding across the ground while rotating - should be enough to start working on the plane

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ extern crate kiss3d;
 extern crate nalgebra;
 
 use nalgebra::na::Vec3;
+use nalgebra::na;
 use kiss3d::window::Window;
+use kiss3d::camera::ArcBall;
 use kiss3d::light;
 
 #[start]
@@ -12,14 +14,27 @@ fn start(argc: int, argv: *const *const u8) -> int {
 }
 
 fn main() {
+    // have camera start from higher position
+    let eye = Vec3::new(5.0, 5.0, 10.0);
+    let at = na::one();
+    let mut arc_ball = ArcBall::new(eye, at);
+
+
     let mut window = Window::new("Kiss3d: cube");
     let mut c      = window.add_cube(1.0, 1.0, 1.0);
+    let mut ground = window.add_quad(10.0, 10.0, 1, 1);
+    ground.set_local_rotation(Vec3::new((90.0f32).to_radians(), 0.0, 0.0));
+    ground.set_color(0.2, 0.2, 0.2);
 
     c.set_color(1.0, 0.0, 0.0);
+    c.set_local_translation(Vec3::new(0.0, 0.5, 0.0));
 
     window.set_light(light::StickToCamera);
 
-    while window.render() {
+    while window.render_with_camera(&mut arc_ball) {
         c.prepend_to_local_rotation(&Vec3::new(0.0f32, 0.014, 0.0));
+        
+        let curr_time = window.context().get_time() as f32;
+        c.set_local_translation(Vec3::new(curr_time.sin(), 0.5f32, curr_time.cos()));
     }
 }
