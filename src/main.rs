b4bf3be85dd4bd4b2e3fdf12adf599c012f39ba7
quote <quote@lv0.io>
2014-08-28 16:56:01 -0500

    vector to handle multiple birds

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate nalgebra;
 
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::rand;
 use nalgebra::na::Vec3;
 use nalgebra::na;
 use kiss3d::window::Window;
@@ -19,7 +20,6 @@ fn start(argc: int, argv: *const *const u8) -> int {
 
 //TODO: check out recorder demo to create mpg's of simulations
 
-
 struct Plane {
     pos: Vec3<f32>,
     vel: Vec3<f32>,
@@ -27,6 +27,7 @@ struct Plane {
 
     node: SceneNode,
 }
+
 impl Plane {
     fn gen_mesh() -> Rc<RefCell<Mesh>> {
         let vertices = vec!(
@@ -50,15 +51,17 @@ impl Plane {
         let mut node = sn;
         node.set_color(1.0, 1.0, 1.0);
         node.enable_backface_culling(false);
-        node.set_local_translation(Vec3::new(0.0f32, 2.0, 0.0));
+        let (x, z) = (rand::random::<f32>() * 5.0 - 2.5, rand::random::<f32>() * 5.0 - 2.5);
+        node.set_local_translation(Vec3::new(x, 2.0, z));
         node.set_points_size(1.0); //wireframe mode for plane
         node.set_lines_width(1.0);
         node.set_surface_rendering_activation(false);
 
+        let (vx, vz) = (rand::random::<f32>() * 4.0 - 2.0, rand::random::<f32>() * 4.0 - 2.0);
 
         Plane {
             pos: Vec3::new(0.0, 2.0, 0.0),
-            vel: Vec3::new(0.5, 0.0, 1.0),
+            vel: Vec3::new(vx, 0.0, vz),
             acc: Vec3::new(0.0, 0.0, 0.0),
             node: node,
         }
@@ -68,12 +71,7 @@ impl Plane {
         let dtv = Vec3::new(dt, dt, dt);
         self.vel = self.vel + self.acc * dtv;
         self.pos = self.pos + self.vel * dtv;
-        self.node.set_local_translation(self.pos);
         self.node.look_at_z(&self.pos, &(self.pos + self.vel), &Vec3::y());
-    }
-
-    fn push(&mut self, v: &Vec3<f32>) {
-        self.vel = self.vel + *v;
     }
 }
 
@@ -87,65 +85,47 @@ fn main() {
 
     let mut window = Window::new("Kiss3d: cube");
     window.set_framerate_limit(Some(60));
+    window.set_light(light::StickToCamera);
 
     let mut ground = window.add_quad(10.0, 10.0, 1, 1);
     ground.set_local_rotation(Vec3::new((90.0f32).to_radians(), 0.0, 0.0));
     ground.set_color(0.2, 0.2, 0.2);
 
-    //let mut c = window.add_cube(1.0, 1.0, 1.0);
-    //c.set_color(1.0, 0.0, 0.0);
-    //c.set_local_translation(Vec3::new(0.0, 0.5, 0.0));
-
     let pmesh = Plane::gen_mesh();
-    let mut p = Plane::new(window.add_mesh(pmesh.clone(), Vec3::new(0.2, 0.2, 0.2)));
 
-    let mut p2 = window.add_mesh(pmesh.clone(), Vec3::new(0.5, 0.5, 0.5));
-    p2.set_color(1.0, 1.0, 1.0);
-    p2.enable_backface_culling(false);
-    p2.set_local_translation(Vec3::new(0.0f32, 2.0, 0.0));
-    p2.set_points_size(1.0); //wireframe mode for plane
-    p2.set_lines_width(1.0);
-    p2.set_surface_rendering_activation(false);
-
-    window.set_light(light::StickToCamera);
+    let mut ps = Vec::new();
+    for i in range(0i, 20) {
+        ps.push(Plane::new(window.add_mesh(pmesh.clone(), Vec3::new(0.2, 0.2, 0.2))));
+    }
 
     let mut last_time = window.context().get_time() as f32;
     let mut curr_time;
     while window.render_with_camera(&mut arc_ball) {
+        curr_time = window.context().get_time() as f32;
+
         draw_axis(&mut window);
 
-        curr_time = window.context().get_time() as f32;
-        //c.prepend_to_local_rotation(&Vec3::new(0.0f32, 0.014, 0.0));
-        //c.set_local_translation(Vec3::new(curr_time.sin(), 0.5f32, curr_time.cos()));
+        for p in ps.mut_iter() {
+            p.update(curr_time - last_time);
 
-        p.update(curr_time - last_time);
+            p.acc.x =
+                if p.pos.x > 5.0 {
+                    -2.0
+                } else if p.pos.x < -5.0 {
+                    2.0
+                } else {
+                    0.0
+                };
 
-        let csin = curr_time.sin();
-        let ccos = curr_time.cos();
-        p2.set_local_translation(Vec3::new(csin * 0.5 + 1.0, ccos * 0.5 + 1.0, csin * 0.5));
-        if csin > 0.0 {
-            p2.prepend_to_local_rotation(&Vec3::new((0.1f32).to_radians() * ccos, 0.0, 0.0));
-        } else {
-            p2.prepend_to_local_rotation(&Vec3::new((-0.1f32).to_radians() * -ccos, 0.0, 0.0));
+            p.acc.z =
+                if p.pos.z > 5.0 {
+                    -2.0
+                } else if p.pos.z < -5.0 {
+                    2.0
+                } else {
+                    0.0
+                };
         }
-
-        p.acc.x =
-            if p.pos.x > 5.0 {
-                -1.0
-            } else if p.pos.x < -5.0 {
-                1.0
-            } else {
-                0.0
-            };
-
-        p.acc.z =
-            if p.pos.z > 5.0 {
-                -1.0
-            } else if p.pos.z < -5.0 {
-                1.0
-            } else {
-                0.0
-            };
 
         last_time = curr_time;
     }
